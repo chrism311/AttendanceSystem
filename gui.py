f4b1d2209b727abae8ac48a2a6dd0da4b87737a7
chrism311 <smchris311@gmail.com>
2019-01-22 02:48:35 -0600

    Added dropbox

--- gui.py
+++ gui.py
@@ -1,33 +1,41 @@
 import sys
+import os
 from main import main, arguments
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
-def window():
-	app = QApplication([])
-	win = QDialog()
-	win.setGeometry(100,100,400,200)
+class window(QWidget):
+	def __init__(self, parent = None):
+		super(window, self).__init__(parent)
 
-	button = QPushButton(win)
-	button.setText("Start Program")
-	button.move(150,70)
-	button.clicked.connect(program)
+		layout = QHBoxLayout()						#Establishes layout for the main window
+		self.btnStrt = QPushButton("Start Program")			#Creates 'Start Program' button and connects signal to event
+		self.btnStrt.clicked.connect(self.program)			#Event for start button is the main program
+		layout.addWidget(self.btnStrt)
 
-	q_button = QPushButton(win)
-	q_button.setText("Quit Program")
-	q_button.move(153,105)
-	q_button.clicked.connect(quit)
+		self.btnStop = QPushButton("Quit Program")
+		self.btnStop.clicked.connect(self.close)
+		layout.addWidget(self.btnStop)
 
-	win.setWindowTitle("Attendance System")
+		self.db = QComboBox()						#Creates dropbox with items being the class directories
+		self.db.addItems(os.listdir("./ids/"))
+		layout.addWidget(self.db)
+
+		self.setLayout(layout)						#Cosmetic changes to window
+		self.setWindowTitle("Attendance System")
+		self.setGeometry(300, 300, 300, 150)
+	
+	def program(self):							#Wrapper function for main program with 'id' path tied to dropbox
+		self.path = './ids/'+ self.db.currentText()			
+		self.args = arguments('./model/20170512-110547/20170512-110547.pb', self.path, '1.0')
+		main(self.args)
+	
+def gui():
+	app = QApplication(sys.argv)						
+	win = window()								#Main window instance
 	win.show()
 	sys.exit(app.exec_())
 
-def program():
-	main(args)
-
-
 if __name__ == '__main__':
-	args = arguments('./model/20170512-110547/20170512-110547.pb', './ids', '1.0')
-	
-	window()
+	gui()
