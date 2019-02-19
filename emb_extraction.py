c758cf778ff7cb6f5d03dba9c2c7b987b82ccecc
chrism311 <smchris311@gmail.com>
2019-02-18 22:10:48 -0600

    cleaned/fixed 'no face' error

--- emb_extraction.py
+++ emb_extraction.py
@@ -17,14 +17,6 @@ import glob
 def Main(args):
     train_set = get_dataset(args.data_dir)
     image_list, label_list = get_image_paths_and_labels(train_set)
-    # fetch the classes (labels as strings) exactly as it's done in get_dataset
-    path_exp = os.path.expanduser(args.data_dir)
-    classes = [path for path in os.listdir(path_exp) \
-               if os.path.isdir(os.path.join(path_exp, path))]
-    classes.sort()
-    # get the label strings
-    label_strings = [name for name in classes if \
-       os.path.isdir(os.path.join(path_exp, name))]
 
     with tf.Graph().as_default():
 
@@ -48,7 +40,8 @@ def Main(args):
                 nrof_batches = (nrof_images // batch_size) + 1
             print('Number of batches: ', nrof_batches)
             embedding_size = embeddings.get_shape()[1]
-            emb_array = np.zeros((nrof_images, embedding_size))
+            emb_array = np.empty((0,embedding_size), int)
+            id_labels = np.array([]) 
             start_time = time.time()
 
             for i in range(nrof_batches):
@@ -57,23 +50,20 @@ def Main(args):
                 else:
                     n = i*batch_size + batch_size
                 # Get images for the batch
-                images = load_and_align_data(image_list[i*batch_size:n], 160, 44, 1.0)
+                images , id_names= load_and_align_data(image_list[i*batch_size:n], 160, 44, 1.0)
                 feed_dict = { images_placeholder: images, phase_train_placeholder:False }
+                id_labels = np.append(id_labels, id_names)
                 # Use the facenet model to calcualte embeddings
                 embed = sess.run(embeddings, feed_dict=feed_dict)
-                emb_array[i*batch_size:n, :] = embed
+                emb_array = np.append(emb_array, embed, axis=0)
                 print('Completed batch', i+1, 'of', nrof_batches)
-
             run_time = time.time() - start_time
             print('Run time: ', run_time)
 
             #   export emedings and labels
-            label_list  = np.array(label_list)
 
             np.save(args.embeddings_name, emb_array)
-            np.save(args.labels_name, label_list)
-            label_strings = np.array(label_strings)
-            np.save(args.labels_strings_name, label_strings[label_list])
+            np.save(args.labels_strings_name, id_labels)
 
 def get_image_paths(images_dir):
     image_paths = []
@@ -111,26 +101,30 @@ def load_and_align_data(image_paths, image_size, margin, gpu_memory_fraction):
         sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
         with sess.as_default():
             mtcnn = create_mtcnn(sess, None)
-
-    nrof_samples = len(image_paths)
-    img_list = [None] * nrof_samples
-    for i in xrange(nrof_samples):
-        print(image_paths[i])
-        img = misc.imread(os.path.expanduser(image_paths[i]), mode='RGB')
-        img_size = np.asarray(img.shape)[0:2]       
+    aligned_images = []
+    id_image_paths = []
+    id_names = []
+    for i in image_paths:       
+        print(i)
+        img = misc.imread(os.path.expanduser(i), mode='RGB')
+        img_size = np.asarray(img.shape)[0:2]
         bounding_boxes, _ = detect_face(img, mtcnn['pnet'], mtcnn['rnet'], mtcnn['onet'])
-        det = np.squeeze(bounding_boxes[0,0:4])
-        bb = np.zeros(4, dtype=np.int32)
-        bb[0] = np.maximum(det[0]-margin/2, 0)
-        bb[1] = np.maximum(det[1]-margin/2, 0)
-        bb[2] = np.minimum(det[2]+margin/2, img_size[1])
-        bb[3] = np.minimum(det[3]+margin/2, img_size[0])
-        cropped = img[bb[1]:bb[3],bb[0]:bb[2],:]
-        aligned = misc.imresize(cropped, (image_size, image_size), interp='bilinear')
-        prewhitened = prewhiten(aligned)
-        img_list[i] = prewhitened
-    images = np.stack(img_list)
-    return images
+        if bounding_boxes.shape[0] > 0:
+            det = np.squeeze(bounding_boxes[0,0:4])
+            bb = np.zeros(4, dtype=np.int32)
+            bb[0] = np.maximum(det[0]-margin/2, 0)
+            bb[1] = np.maximum(det[1]-margin/2, 0)
+            bb[2] = np.minimum(det[2]+margin/2, img_size[1])
+            bb[3] = np.minimum(det[3]+margin/2, img_size[0])
+            cropped = img[bb[1]:bb[3],bb[0]:bb[2],:]
+            aligned = misc.imresize(cropped, (image_size, image_size), interp='bilinear')
+            prewhitened = prewhiten(aligned)
+            aligned_images.append(prewhitened)
+            id_image_paths += i
+            id_names += [i.split('/')[-2]]
+            print('Face recognized: ', id_names[-1])
+    images = np.stack(aligned_images)
+    return images, np.stack(id_names)
 
 class ImageClass():
     #Stores the paths to images for a given class
@@ -154,5 +148,5 @@ class emb_args():
 
 
 if __name__ == '__main__':
-    args = emb_args('./model/20170512-110547.pb', './ids/EENG_4315/', 'embeddings', 'labels', 'labels_strings')
+    args = emb_args('./model/20170512-110547.pb', './ids/missing_pic/', 'embeddings', 'labels', 'labels_strings')
     Main(args)
